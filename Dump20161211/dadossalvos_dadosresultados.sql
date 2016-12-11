854b1309f5a7577f953c05c080379966fdc7d9d6
Rodolfo Fonseca <Rodolfo Fonseca@RF-PC>
2016-12-11 02:04:39 -0200

    Com DadosDoBanco

---
+++ Dump20161211/dadossalvos_dadosresultados.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dadossalvos
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `dadosresultados`
+--
+
+LOCK TABLES `dadosresultados` WRITE;
+/*!40000 ALTER TABLE `dadosresultados` DISABLE KEYS */;
+INSERT INTO `dadosresultados` VALUES (209,'Coluna - 12 -1','Ganho','V','-','-',1),(210,'Coluna - 12 -1','Ganho','V','-','-',1),(211,'Coluna - 12 -2','Ganho','P','V','-',2),(212,'Coluna - 12 -2','Ganho','P','V','-',2),(213,'Coluna - 12-3','Ganho','P','P','V',3),(214,'Coluna - 12-3','Ganho','P','P','V',3),(215,'Coluna - 12-4','Perdido','P','P','P',3),(216,'Coluna - 12-4','Perdido','P','P','P',3);
+/*!40000 ALTER TABLE `dadosresultados` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-12-11  2:00:36
