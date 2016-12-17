41450a73c23beecb4953387ea4a10a72555dc52e
andreqbs <andreqbs@localhost>
2016-12-17 15:10:17 -0300

    Banco de Dados - Atualização

--- src/br/com/roleta/recursos/Roleta.sql
+++ src/br/com/roleta/recursos/Roleta.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema roletaDB
+-- Schema roleta
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema roletaDB
+-- Schema roleta
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `roletaDB` DEFAULT CHARACTER SET utf8 ;
-USE `roletaDB` ;
+CREATE SCHEMA IF NOT EXISTS `roleta` DEFAULT CHARACTER SET utf8 ;
+USE `roleta` ;
 
 -- -----------------------------------------------------
--- Table `roletaDB`.`TipoAposta`
+-- Table `roleta`.`TipoAposta`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `roletaDB`.`TipoAposta` (
+CREATE TABLE IF NOT EXISTS `roleta`.`TipoAposta` (
   `idTipoAposta` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idTipoAposta`))
@@ -25,9 +25,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `roletaDB`.`Usuario`
+-- Table `roleta`.`Usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `roletaDB`.`Usuario` (
+CREATE TABLE IF NOT EXISTS `roleta`.`Usuario` (
   `idUsuario` INT NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idUsuario`))
@@ -35,45 +35,102 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `roletaDB`.`Aposta`
+-- Table `roleta`.`Aposta`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `roletaDB`.`Aposta` (
+CREATE TABLE IF NOT EXISTS `roleta`.`Aposta` (
   `idAposta` INT NOT NULL AUTO_INCREMENT,
   `valorAposta` INT NOT NULL,
-  `statusAposta` INT NULL,
+  `status` INT NULL,
   `idTipoAposta` INT NOT NULL,
   `idUsuario` INT NOT NULL,
   `retorno` INT NULL,
-  `data` DATETIME NOT NULL,
   PRIMARY KEY (`idAposta`),
   INDEX `fk_Aposta_TipoAposta_idx` (`idTipoAposta` ASC),
   INDEX `fk_Aposta_Usuario1_idx` (`idUsuario` ASC),
   CONSTRAINT `fk_Aposta_TipoAposta`
     FOREIGN KEY (`idTipoAposta`)
-    REFERENCES `roletaDB`.`TipoAposta` (`idTipoAposta`)
+    REFERENCES `roleta`.`TipoAposta` (`idTipoAposta`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Aposta_Usuario1`
     FOREIGN KEY (`idUsuario`)
-    REFERENCES `roletaDB`.`Usuario` (`idUsuario`)
+    REFERENCES `roleta`.`Usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `roletaDB`.`NumeroSorteado`
+-- Table `roleta`.`Cassino`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `roletaDB`.`NumeroSorteado` (
+CREATE TABLE IF NOT EXISTS `roleta`.`Cassino` (
+  `idCassino` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idCassino`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `roleta`.`Roleta`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `roleta`.`Roleta` (
+  `idRoleta` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `idCassino` INT NOT NULL,
+  `valorMinino` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idRoleta`),
+  INDEX `fk_Roleta_Cassino1_idx` (`idCassino` ASC),
+  CONSTRAINT `fk_Roleta_Cassino1`
+    FOREIGN KEY (`idCassino`)
+    REFERENCES `roleta`.`Cassino` (`idCassino`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `roleta`.`NumeroSorteado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `roleta`.`NumeroSorteado` (
   `idNumeros` INT NOT NULL AUTO_INCREMENT,
-  `numero` INT NOT NULL,
+  `numero` VARCHAR(45) NOT NULL,
   `dataHora` DATETIME NOT NULL,
   `idUsuario` INT NOT NULL,
+  `idRoleta` INT NOT NULL,
   PRIMARY KEY (`idNumeros`),
   INDEX `fk_NumeroSorteado_Usuario1_idx` (`idUsuario` ASC),
+  INDEX `fk_NumeroSorteado_Roleta1_idx` (`idRoleta` ASC),
   CONSTRAINT `fk_NumeroSorteado_Usuario1`
     FOREIGN KEY (`idUsuario`)
-    REFERENCES `roletaDB`.`Usuario` (`idUsuario`)
+    REFERENCES `roleta`.`Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_NumeroSorteado_Roleta1`
+    FOREIGN KEY (`idRoleta`)
+    REFERENCES `roleta`.`Roleta` (`idRoleta`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `roleta`.`Fundos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `roleta`.`Fundos` (
+  `idUsuario` INT NOT NULL,
+  `idCassino` INT NOT NULL,
+  `valorInicial` INT NULL,
+  `valorAtual` INT NULL,
+  INDEX `fk_Fundos_Usuario1_idx` (`idUsuario` ASC),
+  INDEX `fk_Fundos_Cassino1_idx` (`idCassino` ASC),
+  CONSTRAINT `fk_Fundos_Usuario1`
+    FOREIGN KEY (`idUsuario`)
+    REFERENCES `roleta`.`Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Fundos_Cassino1`
+    FOREIGN KEY (`idCassino`)
+    REFERENCES `roleta`.`Cassino` (`idCassino`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
